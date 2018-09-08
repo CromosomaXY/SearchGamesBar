e97015e69bf354563bb334f359b4c982968525fe
CromosomaXY <pisha666@gmail.com>
2018-09-08 02:11:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pisha/searchgamesbar/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.pisha.searchgamesbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewGames);
+        ArrayList<String> arrayGames = new ArrayList<>();
+        arrayGames.addAll(Arrays.asList(getResources().getStringArray(R.array.array_games)));
+
+        adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayGames);
+
+        listView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
